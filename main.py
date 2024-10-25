7562e74a7eb4d8ce0156fce4584a12d4b98d5cb0
unknown <voltagnelly@gmail.com>
2024-10-25 16:48:30 -0300

    add form

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+import subprocess
 def mostrar_nombre():
     nombre=entrada.get()
     etiqueta.config(text=f"Hola ,{nombre}")
+
+def metodo_propina():
+    root.destroy()
+    subprocess.run(['python',"calculadora_propinas.py"])
+
+def def_formulario():
+    root.destroy()
+    subprocess.run(['python','form.py'])
 
 root=tk.Tk()
 root.geometry("300x200")
@@ -16,5 +25,11 @@ entrada.pack()
 
 boton=tk.Button(root,text="Saludar", command=mostrar_nombre)
 boton.pack()
+
+boton_link=tk.Button(root,text="Calculadora de Propinas", command=metodo_propina)
+boton_link.pack()
+
+boton_formulario=tk.Button(text="Ingresar Datos", command=def_formulario)
+boton_formulario.pack()
 
 root.mainloop()
